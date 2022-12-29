352636f14aa805228d2c1fb530c03e7e56650f58
Naaadddiii <95406520+Naaadddiii@users.noreply.github.com>
2022-12-29 15:28:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,15 +13,45 @@ function App() {
     } else {
       const playersFromSameTeam = plyer.filter(player => player.team_name === dt.team_name).length;
       if (playersFromSameTeam >= 7) {
-        alert(`You have reached the maximum number of players from ${dt.team_name}`);  
-      }
-    const totalCredit = plyer.reduce((total, player) => total + player.event_player_credit, 0);
-    if (totalCredit + dt.event_player_credit > 100) {
-      alert(`You have exceeded the maximum credit limit of 100. The current credit is ${totalCredit}.`);
-     } else {
-        setPlyer([...plyer, dt]);
-      }
-    }
+        alert(`You have reached the maximum number of players from ${dt.team_name}`);
+      }else {
+        if (dt.role === "Batsman") {
+          const batsmenCount = plyer.filter(player => player.role === "Batsman").length;
+          if (batsmenCount > 3 || batsmenCount < 7) {
+            setPlyer([...plyer, dt]);
+          } else {
+            alert(`You have reached the maximum number of batsmen.`);
+          }
+        } else if (dt.role === "Wicket-Keeper") {
+          const wicketkeeperCount = plyer.filter(player => player.role === "Wicket-Keeper").length;
+          if (wicketkeeperCount > 1 || wicketkeeperCount < 5) {
+            setPlyer([...plyer, dt]);
+          } else {
+            alert(`You have reached the maximum number of wicketkeepers.`);
+          }
+        } else if (dt.role === "All-Rounder") {
+          const allRounderCount = plyer.filter(player => player.role === "All-Rounder").length;
+           if ( allRounderCount < 4) {
+           setPlyer([...plyer, dt]);
+        } else {
+            alert(`You have reached the maximum number of all-rounders.`);
+         }
+        } else if (dt.role === "Bowler") {
+            const bowlerCount = plyer.filter(player => player.role === "Bowler").length;
+            if (bowlerCount > 3 || bowlerCount < 7) {
+            setPlyer([...plyer, dt]);
+        } else {
+            alert(`You have reached the maximum number of bowlers.`);
+            }
+       } else {
+             setPlyer([...plyer, dt]);
+       }
+     }
+      const totalCredit = plyer.reduce((total, player) => total + player.event_player_credit, 0);
+      if (totalCredit + dt.event_player_credit > 100) {
+        alert(`You have exceeded the maximum credit limit of 100. The current credit is ${totalCredit}.`);
+      } 
+   }
   };
   return (
     <div>
